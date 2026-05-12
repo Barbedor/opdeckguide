3e54fea504266626652f8c162a200289aff848a4
Yohann <yohannbarbedor@gmail.com>
2026-05-12 09:52:27 +0200

    maj

--- src/lib/site.ts
+++ src/lib/site.ts
@@ -5,19 +5,19 @@ export const siteUrl = "https://opdeckguide.com";
 
 export const staticSitePaths = [
 	"/",
-	"/deck-guides",
-	"/meta-analysis",
-	"/meta-analysis/monkey-d-luffy-eb02-stats-in-op15",
-	"/meta-analysis/nami-op11-stats-in-op15",
-	"/meta-analysis/portgas-d-ace-op15-stats",
-	"/beginner-guides",
-	"/banlist",
-	"/cards-list",
+	"/deck-guides/",
+	"/meta-analysis/",
+	"/meta-analysis/monkey-d-luffy-eb02-stats-in-op15/",
+	"/meta-analysis/nami-op11-stats-in-op15/",
+	"/meta-analysis/portgas-d-ace-op15-stats/",
+	"/beginner-guides/",
+	"/banlist/",
+	"/cards-list/",
 ];
 
 export const guidePaths = [...visibleDeckGuides, ...beginnerGuides].map((guide) => guide.href);
 
-export const cardsListPaths = getExtensions().map((extension) => `/cards-list/${extension}`);
+export const cardsListPaths = getExtensions().map((extension) => `/cards-list/${extension}/`);
 
 export const sitemapPaths = [...staticSitePaths, ...guidePaths, ...cardsListPaths];
 
